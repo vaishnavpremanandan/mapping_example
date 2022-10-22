fda1f32a7dd6f0f376894fc14bedbfa37daa2877
vaishnav premanandan <85191994+vaishnavpremanandan@users.noreply.github.com>
2022-10-22 22:09:22 +0530

    maps in js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Product from "./components/maps";
+
+function App() {
+  return (
+    <div className="App">
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
